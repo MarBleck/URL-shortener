a0782c2bef8694032c660e27b1ed4f2b7cfa57fb
marlonbde <87281287+marlonbde@users.noreply.github.com>
2022-08-09 11:54:37 +0200

    Update init.php

--- init.php
+++ init.php
@@ -31,3 +31,12 @@ $sql2 = "CREATE TABLE url (
     url_user TEXT NOT NULL,
     url_date TEXT NOT NULL
 )";
+$file_pointer = "init.php";
+  
+// Use unlink() function to delete a file
+if (!unlink($file_pointer)) {
+    echo ("$file_pointer cannot be deleted due to an error");
+}
+else {
+    echo ("$file_pointer has been deleted");
+}
